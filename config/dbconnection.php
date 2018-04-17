1a61559bd95ea60f7db342ea9fa30e30f68af2ec
Salaheldin <salaheldin@gmail.com>
2018-04-17 15:09:47 +0200

    refractor dbconnection to implment try,catch

---
+++ config/dbconnection.php
@@ -0,0 +1,12 @@
+<?php 
+
+
+
+try {
+	$dbh = new PDO($dsn, $username, $password, $options);
+	
+} catch (PDOException $e) {
+	$txt = $e->getMessage();
+	$myFile = file_put_contents(connection_errors.txt, $txt, PHP_EOL | LOCK_EX);
+	// exit(header("Location:" . )) 
+}
